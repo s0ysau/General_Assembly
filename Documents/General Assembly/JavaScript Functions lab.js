51338e8cd017126361b6da2f3aaabec2247011e2
Jerrick Lloyd C <jerricklc@gmail.com>
2022-08-02 18:36:02 -0400

    first homework iteration

---
+++ Documents/General Assembly/JavaScript Functions lab.js
@@ -0,0 +1,16 @@
+// 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+  
+  console.log(maxOfTwoNumbers(3, 9));
+  
+  // 2. 
+  
