2a429996ddc9f09864a06bcb6e1165b9e638622d
Jerrick Lloyd C <jerricklc@gmail.com>
2022-08-03 11:19:51 -0400

    Start of process

--- Documents/General Assembly/JavaScript Functions lab.js
+++ Documents/General Assembly/JavaScript Functions lab.js
@@ -1,4 +1,4 @@
-// 1.
+// 1. Define a function, as a function declaration, maxOfTwoNumbersthat takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
 function maxOfTwoNumbers(x, y) {
     if (x >= y) {
       return x;
@@ -12,5 +12,18 @@ function maxOfTwoNumbers(x, y) {
   
   console.log(maxOfTwoNumbers(3, 9));
   
-  // 2. 
+  // 2. Define a function, as a function expression, maxOfThreethat takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
+
+const maxOfThreethat = function (x, y, z){
+    if (x >= y && x >= z){
+        return x;
+    } else if (y >= x && y >= z) {
+        return y;
+    } else {
+        return z;
+    }
+}
+
+
+console.log(maxOfThreethat(10,2,14))
   
