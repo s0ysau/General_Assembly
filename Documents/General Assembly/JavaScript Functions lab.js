53914759dd0c42796c84798a42dcc369effbdc82
Jerrick Lloyd C <jerricklc@gmail.com>
2022-08-03 23:15:20 -0400

    Finished 6/9 questions

--- Documents/General Assembly/JavaScript Functions lab.js
+++ Documents/General Assembly/JavaScript Functions lab.js
@@ -24,6 +24,65 @@ const maxOfThreethat = function (x, y, z){
     }
 }
 
-
-console.log(maxOfThreethat(10,2,14))
+console.log(maxOfThreethat(10,2,15))
   
+//3. Define a function, as a function declaration, isCharAVowelthat takes a character as an argument and returns true if it is a vowel, false otherwise.
+function isCharAVowelthat(x){
+    if (x === "a" || x === "e" || x === "i" || x ===  "o" || x === "u"){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isCharAVowelthat('q'))
+console.log(isCharAVowelthat('e'))
+
+//4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
+let x = 0;
+const sumArray = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        x += arr[i];
+    }
+    return x;
+}
+
+console.log(sumArray([2,4,5]));
+
+//5. Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]); would return 40.
+let y = 1;
+function multiplyArray(arr) {
+    for (let i = 0; i < arr.length; i++){
+        y *= arr[i];
+    }
+    return y;
+}
+console.log(multiplyArray([2,4,5]));
+
+//6. Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
+
+
+//7. Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar'); would return the string "ratskcor".
+//HINT: array.reverse() ... Turn string to array, reverse then back into a string.
+
+function reverseString (string) {
+    let input = string;
+    let convertToArray = input.split('');
+    let reverseArray = convertToArray.reverse();
+    let backToString = reverseArray.toString();
+
+    //https://thispointer.com/remove-comma-from-string-in-javascript/
+    let stringWithoutCommas = backToString.replace(/,/g,'');
+    console.log(stringWithoutCommas);
+}
+
+reverseString('rockstar'); 
+
+//8. Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
+longestStringInArray = () => {
+    let arrayLength = arr.length; 
+}
+
+//9 .Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan (['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"]. 
+
+
