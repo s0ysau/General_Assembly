96f9259139414fd124e09e4459cf6363841bfc54
Jerrick Lloyd C <jerricklc@gmail.com>
2022-08-06 09:12:17 -0400

    Finsihed HW

--- Documents/General Assembly/JavaScript Functions lab.js
+++ Documents/General Assembly/JavaScript Functions lab.js
@@ -1,50 +1,44 @@
 // 1. Define a function, as a function declaration, maxOfTwoNumbersthat takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
-function maxOfTwoNumbers(x, y) {
-    if (x >= y) {
-      return x;
-    } else {
-      return y;
-    }
-  
-    // or more "elegantly" using the fantastic ternary expression!
-    // return  x >= y ? x : y;
-  }
-  
-  console.log(maxOfTwoNumbers(3, 9));
-  
-  // 2. Define a function, as a function expression, maxOfThreethat takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
+
+function maxOfTwoNumbers(x, y){
+    return x >= y ? x : y;
+}
+console.log(maxOfTwoNumbers(3, 9));
+
+// 2. Define a function, as a function expression, maxOfThreethat takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
 
 const maxOfThreethat = function (x, y, z){
-    if (x >= y && x >= z){
-        return x;
-    } else if (y >= x && y >= z) {
-        return y;
-    } else {
-        return z;
-    }
+if (x >= y && x >= z){
+    return x;
+} else if (y >= x && y >= z) {
+    return y;
+} else {
+    return z;
+}
 }
 
 console.log(maxOfThreethat(10,2,15))
-  
+
 //3. Define a function, as a function declaration, isCharAVowelthat takes a character as an argument and returns true if it is a vowel, false otherwise.
 function isCharAVowelthat(x){
-    if (x === "a" || x === "e" || x === "i" || x ===  "o" || x === "u"){
-        return true;
-    } else {
-        return false;
-    }
+if (x === "a" || x === "e" || x === "i" || x ===  "o" || x === "u"){
+    return true;
+} else {
+    return false;
+}
 }
 
 console.log(isCharAVowelthat('q'))
 console.log(isCharAVowelthat('e'))
 
 //4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
+
 let x = 0;
 const sumArray = (arr) => {
-    for (let i = 0; i < arr.length; i++){
-        x += arr[i];
-    }
-    return x;
+for (let i = 0; i < arr.length; i++){
+    x += arr[i];
+}
+return x;
 }
 
 console.log(sumArray([2,4,5]));
@@ -52,31 +46,31 @@ console.log(sumArray([2,4,5]));
 //5. Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]); would return 40.
 let y = 1;
 function multiplyArray(arr) {
-    for (let i = 0; i < arr.length; i++){
-        y *= arr[i];
-    }
-    return y;
+for (let i = 0; i < arr.length; i++){
+    y *= arr[i];
 }
+return y;
+}
+
 console.log(multiplyArray([2,4,5]));
 
 //6. Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
 
-numArgs = () => {
-    return arguments.length;
+numArgs = (...args) => {
+return args.length;
 }
-//console.log(numArgs("Tatoonie", "Asgard", "Titan", 7));
-console.log(numArgs());
+console.log(numArgs("Tatoonie", "Asgard", "Titan", 7, 6, 616));
 
 //7. Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar'); would return the string "ratskcor".
 //HINT: array.reverse() ... Turn string to array, reverse then back into a string.
 
 function reverseString (string) {
-    let input = string;
-    let convertToArray = input.split('');
-    let reverseArray = convertToArray.reverse();
-    let backToString = reverseArray.toString();
-    let stringWithoutCommas = backToString.replace(/,/g,''); // https://thispointer.com/remove-comma-from-string-in-javascript/
-    console.log(stringWithoutCommas);
+let input = string;
+let convertToArray = input.split('');
+let reverseArray = convertToArray.reverse();
+let backToString = reverseArray.toString();
+let stringWithoutCommas = backToString.replace(/,/g,''); // https://thispointer.com/remove-comma-from-string-in-javascript/
+console.log(stringWithoutCommas);
 }
 
 reverseString('California'); 
@@ -85,16 +79,16 @@ reverseString('California');
 //source: https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-125.php#:~:text=function%20longest_str_in_array(arra)%20%7B%20let,%7D%20return%20ans%3B%20%7D%20console.
 
 longestStringInArray = (arr) => {
-    let maxLength = arr[0].length;              //set first element as baseline length
-    let initialLongestString = arr[0];          //set first element as the longest string
-    for (let i = 1; i < arr.length; i++){
-        let a = arr[i].length;                  //goes thru array and set variable for the longest string for if statement
-        if (a > maxLength){                     //determine if the variable 'a' is greater than maxlength
-            initialLongestString = arr[i];      //set the variable if a > maxlength
-            maxLength = a;                      
-        }
+let maxLength = arr[0].length;              //set first element as baseline length
+let initialLongestString = arr[0];          //set first element as the longest string
+for (let i = 1; i < arr.length; i++){
+    let a = arr[i].length;                  //goes thru array and set variable for the longest string for if statement
+    if (a > maxLength){                     //determine if the variable 'a' is greater than maxlength
+        initialLongestString = arr[i];      //set the variable if a > maxlength
+        maxLength = a;                      
     }
-    return initialLongestString;
+}
+return initialLongestString;
 }
 
 console.log(longestStringInArray (['hello', 'Alderaan']));
@@ -103,16 +97,16 @@ console.log(longestStringInArray (['hello', 'Alderaan']));
 //For example, stringsLongerThan (['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"]. 
 
 function stringsLongerThan (arr, num) {
-    let longStrings = [];
-    let properLength = arr[0];
-    for (let i = 0; i < arr.length; i++){
-        let properLength = arr[i].length;
-        if (num <= properLength){
-            properLength = arr[i];
-            longStrings.push(properLength);
-        }
+let longStrings = [];
+let properLength = arr[0];
+for (let i = 0; i < arr.length; i++){
+    let properLength = arr[i].length;
+    if (num <= properLength){
+        properLength = arr[i];
+        longStrings.push(properLength);
     }
-    return longStrings
+}
+return longStrings
 }
 
 console.log(stringsLongerThan(['Greetings', 'world', 'its', 'a', 'great', 'day', 'on', 'earth 616'], 6));
